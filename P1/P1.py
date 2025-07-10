1bc876f942b44642e70fccb45a5da9069dc5986e
Cattatalina <83667101+Cattatalina@users.noreply.github.com>
2025-07-10 15:25:20 -0400

    Add files via upload
    Pregunta 1

---
+++ P1/P1.py
@@ -0,0 +1,6 @@
+# Pregunta 1: Lógica de Programación (30 puntos)
+# Debido a que en el documento no especifica lenguaje, usé Python.
+
+entrada = [4, 2, 7, 2, 4, 9, 1]
+salida = sorted(set(entrada))
+print("Salida =", salida)
